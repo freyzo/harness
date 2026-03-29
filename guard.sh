eecf5c628558158580c0506dbc53f5ae9f6e2f07
Suzy Zou <114107089+freyzo@users.noreply.github.com>
2026-03-28 22:21:05 -0700

    ..

--- guard.sh
+++ guard.sh
@@ -3,9 +3,9 @@
 # Usage: chmod +x guard.sh && ./guard.sh &
 cd "$(dirname "$0")"
 BACKUP=$(cat 0.json)
-H=$(shasum -a 256 0.json | cut -d' ' -f1)
+H=$(cksum 0.json)
 while sleep 2; do
-  N=$(shasum -a 256 0.json | cut -d' ' -f1)
+  N=$(cksum 0.json)
   if [ "$H" != "$N" ]; then
     printf '%s' "$BACKUP" > 0.json
   fi
