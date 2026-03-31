613cd1b20cc3f929fb0db4eeea0ed4a660d04244
Suzy Zou <114107089+freyzo@users.noreply.github.com>
2026-03-30 17:17:00 -0700

    ..

--- guard.sh
+++ guard.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 cd "$(dirname "$0")" || exit 1
 [ -f 0.json ] || exit 1
+if [ "$1" != "--daemon" ]; then
+  "$0" --daemon </dev/null >/dev/null 2>&1 &
+  exit 0
+fi
 BACKUP=$(cat 0.json)
 H=$(cksum 0.json)
 while sleep 2; do
